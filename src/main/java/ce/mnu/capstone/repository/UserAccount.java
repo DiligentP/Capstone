8b727a2caed9402917f967e7e0a1bb2ef3d82b03
DiligentP <qkrwjdgus0603@gmail.com>
2022-07-01 21:46:07 +0900

    JPA 적용중 ERROR 수정중

---
+++ src/main/java/ce/mnu/capstone/repository/UserAccount.java
@@ -0,0 +1,41 @@
+package ce.mnu.capstone.repository;
+
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class UserAccount {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long userno;  // 유저 번호
+    private String userid; // 유저 아이디
+    private String userpass; // 유저 패스워드
+    private String username; // 유저 이름
+    private String userphone; // 유저 폰번호
+    private String useremail; // 유저 이메일
+    private Long userage; // 유저 나이
+    private Date regidate; // 가입일자
+
+    @Builder
+    public UserAccount(String userid, String userpass, String username, String userphone, String useremail, Long userage){
+        this.userid = userid;
+        this.userpass = userpass;
+        this.username = username;
+        this.userphone = userphone;
+        this.useremail = useremail;
+        this.userage = userage;
+        this.regidate = new Date();
+    }
+}
--- src/test/java/ce/mnu/capstone/CapstoneApplicationTests.java
+++ src/test/java/ce/mnu/capstone/CapstoneApplicationTests.java
@@ -1,13 +1,16 @@
 package ce.mnu.capstone;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import ce.mnu.capstone.repository.UserAccountRepository;
+import ce.mnu.capstone.repository.UserAccount;
 
 @SpringBootTest
 class CapstoneApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    @Autowired
+    UserAccountRepository userAccountRepository;
 
 }
